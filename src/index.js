1d9d99b0332b27515983f3cfa28cbad65cd8ec6c
Rodrigo-Arevalo-G <roiarevalo2@gmail.com>
2024-02-29 20:40:30 -0600

    Fixing not javascript error with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';  // Import BrowserRouter for gh-pages
+import { BrowserRouter } from 'react-router-dom/BrowserRouter';  // Import BrowserRouter for gh-pages
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
